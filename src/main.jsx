76d4cf76d4f6959e9837b29c4fe30331a9cc6c09
evripant <evripidispantelaios@gmail.com>
2023-03-18 01:08:13 +0200

    feat: add context providers for homepage and favorite page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+
 import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
 import { theme } from './chakra.theme.js';
+import { ErrorBoundary } from './components/error-boundary/error-boundary';
+import HomepageProvider from './store/homepage/HomepageProvider';
 import './index.css';
+import FavoritesProvider from './store/favorites/FavoritesProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme} backGroundColor="#193131">
-      <App />
-    </ChakraProvider>
+    <ErrorBoundary>
+      <ChakraProvider theme={theme} backGroundColor="#193131">
+        <HomepageProvider>
+          <FavoritesProvider>
+            <App />
+          </FavoritesProvider>
+        </HomepageProvider>
+      </ChakraProvider>
+    </ErrorBoundary>
+    //{' '}
   </React.StrictMode>
 );
