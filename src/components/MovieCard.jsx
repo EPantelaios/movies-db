d9b93e52a3c0ad7e04675cd8a6ce7aca993d1461
evripant <evripidispantelaios@gmail.com>
2023-03-17 20:10:31 +0200

    refactor: improve basic styling for main page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,36 +4,40 @@ import { Box, Image, Flex, Text } from '@chakra-ui/react';
 function MovieCard({ movie }) {
   return (
     <Link to={`/movies/${movie.imdbID}`}>
-      <Flex
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        p={20}
-      >
+      <Flex direction="column" alignItems="center" justifyContent="center">
         <Box
-          p={4}
-          bg="white"
-          boxShadow="md"
-          borderRadius="md"
+          w="clamp(330px, 100%, 450px)"
+          p={3}
+          bg="lightgray"
+          borderRadius="16px"
+          border="2px solid black"
           transition="all 0.2s"
-          _hover={{ boxShadow: 'lg', transform: 'translateY(-4px)' }}
+          _hover={{ boxShadow: 'lg', transform: 'translateY(-5px)' }}
         >
-          <Image
-            src={movie.Poster}
-            alt={movie.Title}
-            borderRadius="md"
-            mb={10}
-            w="full"
-            h="auto"
-          />
           <Flex
+            direction="column"
             justifyContent="center"
             alignItems="center"
             flexWrap="wrap"
-            ml="auto"
+            p={10}
           >
-            <Text fontWeight="bold">{movie.Title}</Text>
-            <Text fontSize="sm">{movie.Year}</Text>
+            {movie.Poster && movie.Poster != 'N/A' && (
+              <Image
+                src={movie.Poster}
+                alt={movie.Title}
+                borderRadius="5px"
+                mb={10}
+                w="full"
+                h="auto"
+                border="1px solid black"
+              />
+            )}
+            <Text fontWeight="bold" textAlign="center">
+              {movie.Title}
+            </Text>
+            <Text fontSize="sm" textAlign="center">
+              {movie.Year}
+            </Text>
           </Flex>
         </Box>
       </Flex>
