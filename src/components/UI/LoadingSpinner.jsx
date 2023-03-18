98d0e3a5c52def1cf1743d2c35bffdaa28e9b2f4
evripant <evripidispantelaios@gmail.com>
2023-03-18 02:46:35 +0200

    remove unnecessary files

--- src/components/UI/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner.jsx
@@ -1,7 +1,41 @@
-import classes from './LoadingSpinner.module.css';
+import { Box, Flex, keyframes } from '@chakra-ui/react';
 
-const LoadingSpinner = () => {
-  return <div className={classes.spinner}></div>;
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+function LoadingSpinner() {
+  return (
+    <Flex
+      margin="3rem auto"
+      textAlign="center"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+    >
+      <Box
+        display="inline-block"
+        width="80px"
+        height="80px"
+        _after={{
+          content: '""',
+          display: 'block',
+          width: '64px',
+          height: '64px',
+          margin: '8px',
+          borderRadius: '50%',
+          border: '6px solid teal',
+          borderColor: 'teal transparent teal transparent',
+          animation: `${spin} 1.2s linear infinite`,
+        }}
+      />
+    </Flex>
+  );
 }
 
 export default LoadingSpinner;
---
+++ src/pages/NotFoundPage/NotFound.jsx
@@ -0,0 +1,26 @@
+import { Flex, Heading } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+import MainButton from '../../components/UI/MainButton';
+
+const NotFound = () => {
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      h="100vh"
+    >
+      <Heading as="h1" size="xl" mb="6">
+        No match for this page...
+      </Heading>
+      <Link to="/">
+        <MainButton colorScheme="teal" fontWeight="bold">
+          Return to homepage
+        </MainButton>
+      </Link>
+    </Flex>
+  );
+};
+
+export default NotFound;
