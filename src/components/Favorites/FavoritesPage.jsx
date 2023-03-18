fdeea97161a0adc802a191be4fbe1a727753167b
evripant <evripidispantelaios@gmail.com>
2023-03-18 17:41:41 +0200

    feat: create a favorite list of movies

---
+++ src/components/Favorites/FavoritesPage.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+
+import { Heading, Flex, Stack } from '@chakra-ui/react';
+
+import FavoritesContext from '../../store/favorites/FavoritesContext';
+import MovieCard from '../HomePage/MovieCard';
+
+function FavoritesPage() {
+  const { favorites } = useContext(FavoritesContext);
+
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      gap="10"
+      p="10"
+    >
+      <Heading as="h1">List of Favorite Movies</Heading>
+      <Stack spacing={10}>
+        {favorites &&
+          favorites.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+      </Stack>
+    </Flex>
+  );
+}
+
+export default FavoritesPage;
